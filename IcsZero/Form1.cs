3cf784330a1127cae34a089dbeb75feb1f386c20
DarkINFINITE24 <justyiuly@gmail.com>
2021-07-06 13:58:27 +0300

    introducere metoda writeXor0();  si stringuri pt "X" si pt "0"

--- IcsZero/Form1.cs
+++ IcsZero/Form1.cs
@@ -12,31 +12,21 @@ namespace IcsZero
 {
     public partial class Form1 : Form
     {
-        bool PlayerTurn = true;
+        bool _playerTurn = true;
+        private string showX = "X";
+        private string show0 = "0";
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void panel1_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
         private void label1_Click(object sender, EventArgs e)
         {
             if (label1.Text == "")
-                if (PlayerTurn)
-                {
-                    label1.Text = "0";
-                    PlayerTurn = !PlayerTurn;
-                }
-                else
-                {
-                    label1.Text = "1";
-                    PlayerTurn = !PlayerTurn;
-                }
-            if (label1.Text == label2.Text && label2.Text == label3.Text)
+            {
+                writeXor0();
+            }
+            if (label1.Text == label2.Text == label3.Text)
             {
                 if (label1.Text == "0") MessageBox.Show("A castigat Toni");
                 else MessageBox.Show("A castigat Dorel");
@@ -89,16 +79,16 @@ namespace IcsZero
         private void label2_Click(object sender, EventArgs e)
         {
             if (label2.Text == "")
-                if (PlayerTurn)
-            {
-                label2.Text = "0";
-                PlayerTurn = !PlayerTurn;
-            }
-            else
-            {
-                label2.Text = "1";
-                PlayerTurn = !PlayerTurn;
-            }
+                if (_playerTurn)
+                {
+                    label2.Text = "0";
+                    _playerTurn = !_playerTurn;
+                }
+                else
+                {
+                    label2.Text = "1";
+                    _playerTurn = !_playerTurn;
+                }
 
             if (label1.Text == label2.Text && label2.Text == label3.Text)
             {
@@ -128,112 +118,70 @@ namespace IcsZero
                 label8.Text = "";
                 label9.Text = "";
             }
-          
+
         }
 
         // TODO faceti restul sa mearga :(
         private void label3_Click(object sender, EventArgs e)
         {
             if (label3.Text == "")
-                if (PlayerTurn)
-            {
-                label3.Text = "0";
-                PlayerTurn = !PlayerTurn;
-            }
-            else
-            {
-                label3.Text = "1";
-                PlayerTurn = !PlayerTurn;
-            }
+                writeXor0();
         }
 
         private void label6_Click(object sender, EventArgs e)
         {
             if (label6.Text == "")
-                if (PlayerTurn)
-            {
-                label6.Text = "0";
-                PlayerTurn = !PlayerTurn;
-            }
-            else
-            {
-                label6.Text = "1";
-                PlayerTurn = !PlayerTurn;
-            }
+                writeXor0();
         }
 
         private void label5_Click(object sender, EventArgs e)
         {
             if (label5.Text == "")
-                if (PlayerTurn)
-            {
-                label5.Text = "0";
-                PlayerTurn = !PlayerTurn;
-            }
-            else
-            {
-                label5.Text = "1";
-                PlayerTurn = !PlayerTurn;
-            }
+                writeXor0();
         }
 
         private void label4_Click(object sender, EventArgs e)
         {
             if (label4.Text == "")
-                if (PlayerTurn)
-            {
-                label4.Text = "0";
-                PlayerTurn = !PlayerTurn;
-            }
-            else
-            {
-                label4.Text = "1";
-                PlayerTurn = !PlayerTurn;
-            }
+                writeXor0();
         }
 
         private void label9_Click(object sender, EventArgs e)
         {
             if (label9.Text == "")
-                if (PlayerTurn)
             {
-                label9.Text = "0";
-                PlayerTurn = !PlayerTurn;
-            }
-            else
-            {
-                label9.Text = "1";
-                PlayerTurn = !PlayerTurn;
+                writeXor0();
             }
         }
 
         private void label8_Click(object sender, EventArgs e)
         {
             if (label8.Text == "")
-                if (PlayerTurn)
             {
-                label8.Text = "0";
-                PlayerTurn = !PlayerTurn;
-            }
-            else
-            {
-                label8.Text = "1";
-                PlayerTurn = !PlayerTurn;
+                writeXor0();
             }
         }
 
         private void label7_Click(object sender, EventArgs e)
         {
             if (label7.Text == "")
-                if (PlayerTurn)
             {
-                label7.Text = "0";
-                PlayerTurn = !PlayerTurn;
+                writeXor0();
+            }
+
+        }
+
+        private void writeXor0()
+        {
+            if (_playerTurn)
+            {
+                label7.Text = show0;
+                _playerTurn = !_playerTurn;
             }
             else
             {
-                label7.Text = "1";
-                PlayerTurn = !PlayerTurn;
+                label7.Text = showX;
+                _playerTurn = !_playerTurn;
             }
         }
     }
